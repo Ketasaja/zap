7715c63917a61f8aa24390289c33cce420a79277
Ketasaja <ketasaja@proton.me>
2025-01-25 05:02:51 +0000

    Refactor

--- zap/src/output/luau/mod.rs
+++ zap/src/output/luau/mod.rs
@@ -190,7 +190,7 @@ pub trait Output {
 			Ty::Color3 => self.push("Color3"),
 			Ty::Vector2 => self.push("Vector3"),
 			Ty::Vector3 => self.push("Vector3"),
-			Ty::Vector(_, _, _) => self.push("vector"),
+			Ty::Vector(..) => self.push("vector"),
 			Ty::AlignedCFrame => self.push("CFrame"),
 			Ty::CFrame => self.push("CFrame"),
 		}
